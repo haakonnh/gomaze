c0db3073962ca704a77869ca0e7461e4b4423424
Haakon Hellebust <57912883+haakonnh@users.noreply.github.com>
2024-02-08 09:36:08 +0100

    gogo

--- algorithms.go
+++ algorithms.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"image/color"
-	"slices"
-
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/vector"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 type Wall struct {
 	Exists bool // Flag indicating whether the wall exists or not
 	Cell1  *Cell
 	Cell2  *Cell
-	Weight int
 }
 
 type Cell struct {
@@ -26,103 +23,195 @@ type Maze struct {
 	Width  int
 	Height int
 	Cells  [][]*Cell
+	Walls  map[[2]*Cell]*Wall
+}
+
+type Coords struct {
+	X int
+	Y int
 }
 
 func InitMaze(width, height int) *Maze {
-	// Initialize the maze grid
-	cells := make([][]*Cell, height)
-	for i := range cells {
-		cells[i] = make([]*Cell, width)
-		for j := range cells[i] {
-			cells[i][j] = &Cell{
+	maze := &Maze{
+		Width:  width,
+		Height: height,
+		Cells:  make([][]*Cell, height),
+		Walls:  make(map[[2]*Cell]*Wall),
+	}
+	for i := range maze.Cells {
+		maze.Cells[i] = make([]*Cell, width)
+		for j := range maze.Cells[i] {
+			maze.Cells[i][j] = &Cell{
 				Row:      i,
 				Column:   j,
 				Visited:  false,
-				Adjacent: make([]*Cell, 0), // Initialize the slice for adjacent cells
+				Adjacent: make([]*Cell, 0),
 			}
 		}
 	}
+	// Populate adjacent cells and create corresponding walls
+	for i := range maze.Cells {
+		for j := range maze.Cells[i] {
+			cell := maze.Cells[i][j]
 
-	// Create shared walls between adjacent cells
-	for i := range cells {
-		for j := range cells[i] {
-			cell := cells[i][j]
-
-			// Add adjacent cells to the current cell
+			// Populate upper adjacent cell and create wall
 			if i > 0 {
-				cell.Adjacent = append(cell.Adjacent, cells[i-1][j])
+				upperCell := maze.Cells[i-1][j]
+				upperCell.Adjacent = append(upperCell.Adjacent, cell)
+				cell.Adjacent = append(cell.Adjacent, upperCell)
+				wall := &Wall{Exists: true, Cell1: upperCell, Cell2: cell}
+				maze.Walls[[2]*Cell{upperCell, cell}] = wall
+				maze.Walls[[2]*Cell{cell, upperCell}] = wall
 			}
-			if i < height-1 {
-				cell.Adjacent = append(cell.Adjacent, cells[i+1][j])
-			}
+
+			// Populate left adjacent cell and create wall
 			if j > 0 {
-				cell.Adjacent = append(cell.Adjacent, cells[i][j-1])
-			}
-			if j < width-1 {
-				cell.Adjacent = append(cell.Adjacent, cells[i][j+1])
+				leftCell := maze.Cells[i][j-1]
+				leftCell.Adjacent = append(leftCell.Adjacent, cell)
+				cell.Adjacent = append(cell.Adjacent, leftCell)
+				wall := &Wall{Exists: true, Cell1: leftCell, Cell2: cell}
+				maze.Walls[[2]*Cell{leftCell, cell}] = wall
+				maze.Walls[[2]*Cell{cell, leftCell}] = wall
 			}
 
-		}
+			// Populate lower adjacent cell and create wall
+			if i < maze.Height-1 {
+				lowerCell := maze.Cells[i+1][j]
+				lowerCell.Adjacent = append(lowerCell.Adjacent, cell)
+				cell.Adjacent = append(cell.Adjacent, lowerCell)
+				wall := &Wall{Exists: true, Cell1: lowerCell, Cell2: cell}
+				maze.Walls[[2]*Cell{lowerCell, cell}] = wall
+				maze.Walls[[2]*Cell{cell, lowerCell}] = wall
+			}
 
-	}
-	return &Maze{
-		Width:  width,
-		Height: height,
-		Cells:  cells,
-	}
-}
-
-func (m *Maze) DrawMaze(screen *ebiten.Image) {
-	for i := range m.Cells {
-		for j := range m.Cells[i] {
-			if j+1 <= len(m.Cells[i])-1 && slices.Contains(m.Cells[i][j].Adjacent, m.Cells[i][j+1]) {
-				j := float32(j)
-				i := float32(i)
-				vector.StrokeLine(screen, (j+1)*10, i*10, (j+1)*10, (i+1)*10, 1, color.RGBA{0x80, 0x80, 0x80, 0xff}, false)
-			}
-			if i+1 <= len(m.Cells)-1 && slices.Contains(m.Cells[i][j].Adjacent, m.Cells[i+1][j]) {
-				j := float32(j)
-				i := float32(i)
-				vector.StrokeLine(screen, j*10, (i+1)*10, (j+1)*10, (i+1)*10, 1, color.RGBA{0x80, 0x80, 0x80, 0xff}, false)
-			}
-			if j-1 >= 0 && slices.Contains(m.Cells[i][j].Adjacent, m.Cells[i][j-1]) {
-				j := float32(j)
-				i := float32(i)
-				vector.StrokeLine(screen, j*10, i*10, j*10, (i+1)*10, 1, color.RGBA{0x80, 0x80, 0x80, 0xff}, false)
-			}
-			if i-1 >= 0 && slices.Contains(m.Cells[i][j].Adjacent, m.Cells[i-1][j]) {
-				j := float32(j)
-				i := float32(i)
-				vector.StrokeLine(screen, j*10, i*10, (j+1)*10, i*10, 1, color.RGBA{0x80, 0x80, 0x80, 0xff}, false)
+			// Populate right adjacent cell and create wall
+			if j < maze.Width-1 {
+				rightCell := maze.Cells[i][j+1]
+				rightCell.Adjacent = append(rightCell.Adjacent, cell)
+				cell.Adjacent = append(cell.Adjacent, rightCell)
+				wall := &Wall{Exists: true, Cell1: rightCell, Cell2: cell}
+				maze.Walls[[2]*Cell{rightCell, cell}] = wall
+				maze.Walls[[2]*Cell{cell, rightCell}] = wall
 			}
 		}
 	}
+
+	return maze
 }
+func (maze *Maze) Prim() {
+	// Choose the starting cell [0,0] and mark it as visited
+	startRow, startCol := 13, 13
+	currentCell := maze.Cells[startRow][startCol]
+	currentCell.Visited = true
+	unvisitedWalls := maze.getUnvisitedWalls(currentCell)
+	fmt.Println("Unvisited walls: ", len(unvisitedWalls))
+	for len(unvisitedWalls) > 0 {
+		time.Sleep(time.Second)
+		// Choose a random unvisited wall
+		shuffleWalls(unvisitedWalls)
+		randomWall := unvisitedWalls[0]
+		// Get the neighboring cell of the wall
+		neighbor := unvisitedWalls[0].getNeighbor(currentCell)
+		neighbor.Visited = true
 
-type Graph struct {
-	vertices int
-	edges    []Edge
-}
+		// Mark the wall as non-existent
+		randomWall.Exists = false
+		// Because in the map we have the same wall for both cells, we need to mark the wall as non-existent for the neighbor cell as well
+		maze.markWallAsNonExistent(currentCell, neighbor)
 
-type Edge struct {
-	src, dest, weight int
-}
+		// Add the neighboring cell's unvisited walls to the list
+		unvisitedWalls = append(unvisitedWalls, maze.getUnvisitedWalls(neighbor)...)
 
-func Prim(maze *Maze) {
-
-}
-
-func DFS(cell *Cell, goal *Cell) bool {
-	if cell == goal {
-		return true
+		// Remove the current wall from the list
+		unvisitedWalls = unvisitedWalls[1:]
 	}
-	cell.Visited = true
+}
+
+// getUnvisitedWalls returns a list of unvisited walls from the current cell
+func (maze *Maze) getUnvisitedWalls(cell *Cell) []*Wall {
+	unvisitedWalls := make([]*Wall, 0)
+	for _, neighbor := range cell.Adjacent {
+		// Check if the neighbor cell is unvisited
+		if !neighbor.Visited {
+			// Check if the wall between the current cell and its neighbor exists and is unvisited
+			wall := maze.Walls[[2]*Cell{cell, neighbor}]
+			// get coords of the wall
+
+			if wall != nil && wall.Exists && maze.Walls[[2]*Cell{neighbor, cell}].Exists {
+				unvisitedWalls = append(unvisitedWalls, wall)
+			}
+		}
+	}
+	return unvisitedWalls
+}
+
+// Get the walls associated with a cell
+func (maze *Maze) getWalls(cell *Cell) []*Wall {
+	walls := make([]*Wall, 0)
+if cell.Row > 0 {
+		walls = append(walls, maze.Walls[[2]*Cell{cell, maze.Cells[cell.Row-1][cell.Column]}])
+	}
+	if cell.Column > 0 {
+		walls = append(walls, maze.Walls[[2]*Cell{cell, maze.Cells[cell.Row][cell.Column-1]}])
+	}
+	if cell.Row < maze.Height-1 {
+		walls = append(walls, maze.Walls[[2]*Cell{cell, maze.Cells[cell.Row+1][cell.Column]}])
+	}
+	if cell.Column < maze.Width-1 {
+		walls = append(walls, maze.Walls[[2]*Cell{cell, maze.Cells[cell.Row][cell.Column+1]}])
+	}
+
+	return walls
+}
+
+// getNeighbor returns the neighboring cell of a wall
+func (wall *Wall) getNeighbor(cell *Cell) *Cell {
+	if wall.Cell1 == cell {
+		return wall.Cell2
+	}
+	if wall.Cell2 == cell {
+		return wall.Cell1
+	}
+	fmt.Println("Error: the cell is not a neighbor of the wall")
+	return nil
+}
+
+// getUnvisitedNeighbors returns the unvisited neighbors of a cell
+func (maze *Maze) getUnvisitedNeighbors(cell *Cell) []*Cell {
+	unvisitedNeighbors := make([]*Cell, 0)
 	for _, neighbor := range cell.Adjacent {
 		if !neighbor.Visited {
-			if DFS(neighbor, goal) {
-				return true
+			unvisitedNeighbors = append(unvisitedNeighbors, neighbor)
+		}
+	}
+	return unvisitedNeighbors
+}
+
+// markWallAsNonExistent marks the wall between two cells as non-existent
+func (maze *Maze) markWallAsNonExistent(cell1, cell2 *Cell) {
+	for _, neighbor := range cell1.Adjacent {
+		if neighbor == cell2 {
+			// Found the neighbor cell, mark the corresponding wall as non-existent
+			for _, wall := range maze.getWalls(cell1) {
+				if (wall.Cell1 == cell1 && wall.Cell2 == cell2) || (wall.Cell1 == cell2 && wall.Cell2 == cell1) {
+					wall.Exists = false
+					return
+				}
 			}
 		}
 	}
-	return false
+}
+
+// Shuffle the slice of cells to randomize traversal
+func shuffleNeighbors(neighbors []*Cell) {
+	rand.Shuffle(len(neighbors), func(i, j int) {
+		neighbors[i], neighbors[j] = neighbors[j], neighbors[i]
+	})
+}
+
+// Shuffle the slice of walls to randomize traversal
+func shuffleWalls(walls []*Wall) {
+	rand.Shuffle(len(walls), func(i, j int) {
+		walls[i], walls[j] = walls[j], walls[i]
+	})
 }
--- main.go
+++ main.go
@@ -1,22 +1,74 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Game struct{}
 
 func (g *Game) Update() error {
+
+	//maze.Prim()
 	return nil
 }
 
 // Init maze with 100x100 cells
-var maze *Maze = InitMaze(100, 100)
+var maze *Maze = InitMaze(25, 25)
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	maze.DrawMaze(screen)
+	cellSize := 5
+	for i := 0; i < len(maze.Cells); i++ {
+		for j := 0; j < len(maze.Cells[i]); j++ {
+			if maze.Cells[i][j].Visited {
+				vector.DrawFilledRect(screen, float32(j*cellSize-1), float32(i*cellSize), float32(cellSize), float32(cellSize), color.White, false)
+			}
+		}
+	}
+	for _, wall := range maze.Walls {
+		if wall.Exists {
+			cell1 := wall.Cell1
+			cell2 := wall.Cell2
+			x1 := cell1.Column * cellSize
+			y1 := cell1.Row * cellSize
+			x2 := cell2.Column * cellSize
+			y2 := cell2.Row * cellSize
+
+			// Determine the direction of the wall
+			// 0: horizontal, 1: vertical
+			var direction int
+			if cell1.Row == cell2.Row {
+				direction = 0 // horizontal wall
+			} else {
+				direction = 1 // vertical wall
+			}
+
+			// Draw the wall around the cells
+			if direction == 0 {
+				// Horizontal wall
+				if cell1.Column < cell2.Column {
+					// Wall to the right of cell1
+					vector.StrokeLine(screen, float32(x1+cellSize), float32(y1), float32(x1+cellSize), float32(y1+cellSize), 1, color.Black, false)
+				} else {
+					// Wall to the right of cell2
+					vector.StrokeLine(screen, float32(x2+cellSize), float32(y2), float32(x2+cellSize), float32(y2+cellSize), 1, color.Black, false)
+				}
+			} else {
+				// Vertical wall
+				if cell1.Row < cell2.Row {
+					// Wall below cell1
+					vector.StrokeLine(screen, float32(x1), float32(y1+cellSize), float32(x1+cellSize), float32(y1+cellSize), 1, color.Black, false)
+				} else {
+					// Wall below cell2
+					vector.StrokeLine(screen, float32(x2), float32(y2+cellSize), float32(x2+cellSize), float32(y2+cellSize), 1, color.Black, false)
+				}
+			}
+		}
+	}
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -24,10 +76,34 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-
+	//maze.Prim()
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		log.Fatal(err)
+
+	// Create a channel to signal when Prim's algorithm has finished
+	done := make(chan struct{})
+
+	// Run Prim's algorithm in a separate goroutine
+	go func() {
+		maze.Prim()
+		// Signal that Prim's algorithm has finished
+		close(done)
+	}()
+	// Run the main loop to continuously draw the maze
+	for !isDone(done) {
+		// Advance the game state
+		if err := ebiten.RunGame(&Game{}); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+// isDone checks if Prim's algorithm has finished
+func isDone(done <-chan struct{}) bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
 	}
 }
